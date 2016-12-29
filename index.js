80e341bc8428914ab7226a4ca249809321f2042d
John Cole <johnwebbcole@gmail.com>
2016-12-28 18:53:51 -0600

    modified to scan the package.json file for dependencies

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
 var through = require('through2'),
     fs = require('fs'),
     path = require('path'),
-    globby = require('globby');
+    globby = require('globby'),
+    gutil = require('gulp-util');
+
+function getPackage(file, stream, loaded, depth) {
+    depth = depth || 0;
+    if (depth > 1) {
+        throw new Error('getPackage depth exceeded');
+    }
+    // console.log('getPackage file', depth, file.path);
+    var pkg = JSON.parse(file.contents.toString());
+    if (pkg && pkg.dependencies) {
+        Object.keys(pkg.dependencies).forEach(function (key) {
+            // console.log('pkg.dependencies key', key);
+            globby.sync(['node_modules/' + key + '/jscad.json']).forEach(function (jscadconf) {
+                var jscad = JSON.parse(fs.readFileSync(jscadconf));
+                jscad.files.forEach(function (depfilename) {
+                    // console.log('  jscad:', jscadconf, depfilename);
+                    var deppath = path.resolve(path.dirname(jscadconf), depfilename);
+                    var depfile = new gutil.File({
+                        path: deppath,
+                        contents: fs.readFileSync(deppath)
+                    });
+
+                    stream.push(depfile);
+                });
+
+            });
+
+
+        });
+    }
+}
 
 module.exports = function () {
     return through.obj(function (file, encoding, callback) {
-        var jscad = JSON.parse(file.contents.toString());
+        var self = this;
 
-        if (jscad.files) {
-            jscad.files.forEach((f) => {
-                var n = file.clone();
-                n.path = path.resolve(path.dirname(file.path), f);
-
-                if (fs.existsSync(n.path)) {
-                    n.contents = fs.readFileSync(n.path);
-                } else {
-                    // try direct path
-                    n.contents = fs.readFileSync(f);
-                }
-                this.push(n);
-            });
-
-            var pkg = JSON.parse(fs.readFileSync(path.resolve(path.dirname(file.path), 'package.json')));
-
-            if (pkg && pkg.dependencies) {
-                Object.keys(pkg.dependencies).forEach((key) => {
-                    globby(['node_modules/' + key + '/package.json']).then(paths => {
-                        // console.log('globby', key, paths);
-                        var deppkg = JSON.parse(fs.readFileSync(paths[0]));
-                        var d = file.clone();
-                        d.path = path.resolve(path.dirname(paths), deppkg.main);
-                        d.contents = fs.readFileSync(d.path);
-                        // If the main file is `index.js`, change it to the package name, so we don't get a bunch of `index.js` overwrites
-                        if (path.basename(d.path) === 'index.js') d.path = path.resolve(path.dirname(paths), key + '.js');
-                        this.push(d);
-                    });
-                });
-            }
-        }
+        getPackage(file, self, {});
+        // console.warn('cb');
         callback();
+
     });
 };
